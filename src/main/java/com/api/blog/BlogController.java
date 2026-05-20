d3edabe2d09753695d89ca5b081c9a43f3a28006
KARTHICKEYANGV <karthickeyangvv@gmail.com>
2026-05-20 09:03:30 +0530

    Put,GetAll,get functionalities added

--- src/main/java/com/api/blog/BlogController.java
+++ src/main/java/com/api/blog/BlogController.java
@@ -1,11 +1,11 @@
 package com.api.blog;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,8 +15,34 @@ public class BlogController {
 
     //create API
     @PostMapping("/create")
-    public ResponseEntity<BlogEntity> createBlog(@RequestBody BlogEntity blogEntity){
+    public ResponseEntity<BlogEntity> createBlog(@RequestBody BlogEntity blogEntity) {
         BlogEntity savedBlog = blogService.save(blogEntity);
-        return ResponseEntity.ok(savedBlog);
+        return new ResponseEntity<>(savedBlog, HttpStatus.CREATED);
     }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<BlogEntity> getBlog(@PathVariable Long id) {
+        BlogEntity blog = blogService.findById(id);
+        if (blog != null) {
+            return ResponseEntity.ok(blog);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<BlogEntity> updateBlog(@PathVariable Long id, @RequestBody BlogEntity updatedBlog) {
+        BlogEntity blog = blogService.update(id, updatedBlog);
+        if (blog != null) {
+            return ResponseEntity.ok(blog);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<BlogEntity>> getAllBlogs() {
+        return new ResponseEntity<List<BlogEntity>>(blogService.findAll(), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/api/blog/BlogService.java
+++ src/main/java/com/api/blog/BlogService.java
@@ -3,6 +3,8 @@ package com.api.blog;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BlogService {
@@ -10,6 +12,26 @@ public class BlogService {
 
     public BlogEntity save(BlogEntity blogEntity) {
         return blogRepository.save(blogEntity);
+    }
+
+    public BlogEntity findById(Long id) {
+        return blogRepository.findById(id).orElse(null);
+    }
+
+    public BlogEntity update(Long id, BlogEntity blogEntity) {
+        BlogEntity existingBlog = blogRepository.findById(id).orElse(null);
+        if (existingBlog != null) {
+            existingBlog.setTitle(blogEntity.getTitle());
+            existingBlog.setContent(blogEntity.getContent());
+            existingBlog.setCategory(blogEntity.getCategory());
+            existingBlog.setTags(blogEntity.getTags());
+            return blogRepository.save(existingBlog);
+        }
+        return null;
+    }
+
+    public List<BlogEntity> findAll() {
+        return blogRepository.findAll();
     }
 
 }
